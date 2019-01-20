fe1061b2890a5a1cf3ee05022dae65234da7636a
Pieter <censornv@gmail.com>
2019-01-20 16:36:44 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { DragDropListComponent } from './dragdroplist/dragdroplist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
